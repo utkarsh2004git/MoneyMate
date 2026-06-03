195b1c5c8506df650b09c5230db08a22ae174b51
Utkarsh Shahare <utkarshshahare2004@gmail.com>
2026-06-03 17:48:39 +0530

    Add JWT request filter and update security configuration to include it

--- backend/src/main/java/com/moneymate/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/moneymate/backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +14,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.moneymate.backend.security.AppUserDetailsService;
+import com.moneymate.backend.security.JwtRequestFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,14 +29,15 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final AppUserDetailsService appUserDetailsService;
-
+    private final JwtRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.cors(Customizer.withDefaults())
             .csrf(csrf->csrf.disable())
             .authorizeHttpRequests(auth->auth.requestMatchers("/health","/status","/login","/register","/activate").permitAll()
             .anyRequest().authenticated())
-            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
