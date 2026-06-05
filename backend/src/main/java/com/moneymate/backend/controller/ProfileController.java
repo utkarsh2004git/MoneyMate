e4f3e0282f61c345be37c168d7ea75b1f3a1d57e
Utkarsh Shahare <utkarshshahare2004@gmail.com>
2026-06-06 02:24:07 +0530

    feat: add endpoint to retrieve public profile information

--- backend/src/main/java/com/moneymate/backend/controller/ProfileController.java
+++ backend/src/main/java/com/moneymate/backend/controller/ProfileController.java
@@ -59,5 +59,11 @@ public class ProfileController {
         }
     }
 
+    @GetMapping("/profile")
+    public ResponseEntity<ProfileDTO> getPublicProfile(){
+        ProfileDTO profileDTO = profileService.getPublicProfile(null);
+        return ResponseEntity.ok(profileDTO);
+    }
+
 
 }
