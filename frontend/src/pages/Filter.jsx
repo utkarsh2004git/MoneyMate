e732a085675a8cd28af1c4622d7c2895dc680f10
Utkarsh Shahare <utkarshshahare2004@gmail.com>
2026-06-06 23:46:59 +0530

    feat: implement transaction filtering functionality; add filter form and API integration for transactions

--- frontend/src/pages/Filter.jsx
+++ frontend/src/pages/Filter.jsx
@@ -1,16 +1,205 @@
-import React from 'react'
-import Dashboard from '../components/Dashboard'
-import useUser from '../hooks/useUser';
-
+import React, { useState } from "react";
+import Dashboard from "../components/Dashboard";
+import useUser from "../hooks/useUser";
+import { Search } from "lucide-react";
+import axiosConfig from "../util/axiosConfig";
+import { API_ENDPOINTS } from "../util/apiEndpoints";
+import { toast } from "react-hot-toast";
+import TransactionalInfoCard from "../components/TransactionalInfoCard";
+import moment from "moment";
 const Filter = () => {
   useUser();
+  const [type, setType] = useState("income");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [sortField, setSortField] = useState("date");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await axiosConfig.post(API_ENDPOINTS.APPLY_FILTERS, {
+        type,
+        startDate,
+        endDate,
+        keyword,
+        sortField,
+        sortOrder,
+      });
+      setTransactions(res.data);
+      // console.log(res.data);
+    } catch (error) {
+      console.error("Failed to fetch the transaction: ", error);
+      toast.error(
+        error.response?.data?.message ||
+          "Failed to fetch transactions. Please try again.",
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <Dashboard activeMenu={"Filter"}>
-        This is Filter page.
+        <div className="my-5 mx-auto ">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-semibold">Filter Transactions</h2>
+          </div>
+          <div className="bg-white p-4 mb-4">
+            <div className="flex items-center justify-between mb-4">
+              <h5 className="text-lg font-semibold ">Select the filters</h5>
+            </div>
+            <form className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-6 gap-4">
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="type"
+                >
+                  Type
+                </label>
+                <select
+                  value={type}
+                  onChange={(e) => setType(e.target.value)}
+                  id="type"
+                  className="w-full border rounded px-3 py-2"
+                >
+                  <option value="income">Income</option>
+                  <option value="expense">Expense</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="startdate"
+                >
+                  Start Date
+                </label>
+                <input
+                  value={startDate}
+                  onChange={(e) => setStartDate(e.target.value)}
+                  id="startdate"
+                  type="date"
+                  className="w-full border rounded px-3 py-2 "
+                />
+              </div>
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="enddate"
+                >
+                  End Date
+                </label>
+                <input
+                  value={endDate}
+                  onChange={(e) => setEndDate(e.target.value)}
+                  id="enddate"
+                  type="date"
+                  className="w-full border rounded px-3 py-2 "
+                />
+              </div>
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="sortfield"
+                >
+                  Sort Field
+                </label>
+                <select
+                  value={sortField}
+                  onChange={(e) => setSortField(e.target.value)}
+                  id="sortfield"
+                  className="w-full border rounded px-3 py-2"
+                >
+                  <option value="date">Date</option>
+                  <option value="amount">Amount</option>
+                  <option value="category">Category</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  className="block text-sm font-medium mb-1"
+                  htmlFor="sortorder"
+                >
+                  Sort Order
+                </label>
+                <select
+                  value={sortOrder}
+                  onChange={(e) => setSortOrder(e.target.value)}
+                  id="sortorder"
+                  className="w-full border rounded px-3 py-2"
+                >
+                  <option value="asc">Ascending</option>
+                  <option value="desc">Descending</option>
+                </select>
+              </div>
+              <div className="sm:col-span-1 md:col-span-2 flex items-end">
+                <div className="w-full">
+                  <label
+                    className="block text-sm font-medium mb-1"
+                    htmlFor="keyword"
+                  >
+                    Search
+                  </label>
+                  <input
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                    id="keyword"
+                    type="text"
+                    placeholder="Search..."
+                    className="w-full border rounded px-3 py-2"
+                  />
+                </div>
+                <button
+                  onClick={handleSearch}
+                  className="ml-2 mb-1 p-2 duration-200 bg-blue-500 hover:bg-blue-800 text-white rounded flex items-center justify-center cursor-pointer"
+                >
+                  <Search size={20} />
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="bg-white p-4">
+            <div className="flex justify-between items-center mb-4">
+              <h5 className="text-2xl font-semibold">Transactions</h5>
+            </div>
+            {transactions.length === 0 && !loading ? (
+              <>
+                <p className="text-gray-500">
+                  Select the filters and click apply to filter the transactions.
+                </p>
+              </>
+            ) : (
+              ""
+            )}
+            {loading ? (
+              <>
+                {" "}
+                <p className="text-gray-500">Loading Transactions...</p>
+              </>
+            ) : (
+              ""
+            )}
+            {transactions.map((t) => (
+              <TransactionalInfoCard 
+              key={t.id} 
+              title={t.name} 
+              icon={t.icon} 
+              date={moment(t.date).format("Do MMM YYYY")}
+              amount={t.amount}
+              type={type}
+              hideDeleteBtn={true}
+              />
+            ))}
+          </div>
+        </div>
       </Dashboard>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
--- frontend/src/util/apiEndpoints.js
+++ frontend/src/util/apiEndpoints.js
@@ -12,11 +12,14 @@ export const API_ENDPOINTS = {
     ADD_CATEGORY : "/categories",
     GET_CATEGORY_BY_TYPE : (type)=>`/categories/${type}`,
     UPDATE_CATEGORY : (categoryId)=>`/categories/${categoryId}`,
+
     GET_ALL_INCOMES : "/incomes",
     ADD_INCOME : `/incomes`,
     DELETE_INCOME : (incomeId)=>`/incomes/${incomeId}`,
     INCOME_EXCEL_DOWNLOAD :`/excel/download/income`,
     EMAIL_INCOME :`/email/income-excel`,
+
+    APPLY_FILTERS :`/filter`,
     UPLOAD_IMAGE:`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
     
 }
